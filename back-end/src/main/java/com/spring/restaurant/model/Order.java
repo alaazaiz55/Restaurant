986cf36827ab72d9abd86ac59fded0478f1cc940
alaazaiz55 <azaizala95@gmail.com>
2023-09-26 12:42:16 +0200

    create model (Category and Order) Closes #1

---
+++ back-end/src/main/java/com/spring/restaurant/model/Order.java
@@ -0,0 +1,11 @@
+package com.spring.restaurant.model;
+
+import org.apache.tomcat.util.scan.StandardJarScanFilter;
+
+public class Order {
+  private Long id;
+  private String nameOrder;
+  private int price;
+  private String img;
+  private String description;
+}
