2c40359501ad71049ce3f32ba1ba7d051fbeb409
alaazaiz55 <azaizala95@gmail.com>
2023-09-26 13:02:52 +0200

    lombok #2

--- back-end/src/main/java/com/spring/restaurant/model/Category.java
+++ back-end/src/main/java/com/spring/restaurant/model/Category.java
@@ -1,5 +1,12 @@
 package com.spring.restaurant.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Category {
 
     private Long id;
